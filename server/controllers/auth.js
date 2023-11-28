eeb299fe6e2274649b8a4b38186c3ff8c42d0f64
Manuel Junior <manuel.jr@outlook.com>
2023-11-28 01:33:06 +0000

    Text1

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-import { conn, gfs } from "../index.js";
+import { gfs } from "../index.js";
 
 /* REGISTER USER */
 export const register = async (req, res) => {
